681183fcdd59981fe8b880a736a7c7900ced4293
Maximiliano Ariel Giovine <maxiagiovine@gmail.com>
2025-04-14 07:54:18 -0300

    agregado de babel e incorporación de nuevas dependencias

--- node_modules/frontend/webpack.config.js
+++ node_modules/frontend/webpack.config.js
@@ -1,8 +1,23 @@
+const path = require('path');
+
 module.exports = {
-    entry : "./index.js",
-    mode : "development",
-    output : {
-        path : __dirname + "/public",
-        filename : "bundle.js",
-    }
-}
+  entry: './index.js', // Archivo de entrada principal
+  output: {
+    path: path.resolve(__dirname, 'public'), // Cambiado a 'public'
+    filename: 'bundle.js', // Nombre del archivo generado
+  },
+  mode: 'production', // Modo de construcción
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Manejo de archivos CSS
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/, // Manejo de archivos JS/JSX
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
