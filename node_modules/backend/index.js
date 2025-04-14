de41364d3ad9905df937562672013846084923af
Maximiliano Ariel Giovine <maxiagiovine@gmail.com>
2025-04-14 19:33:49 -0300

    cambio de modules, se quitó el backend

--- node_modules/backend/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./src/server')
-const dbcon = require('./src/config/dbcon') // Import the dbcon function to establish a connection to the database
-
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-   dbcon() // Call the dbcon function to establish a connection to the database
-})
--- node_modules/backend/src/config/dotenvConfig.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const dotenv = require('dotenv');
-
-// Configura dotenv para cargar el archivo .env desde la carpeta config
-dotenv.config({ path: path.resolve(__dirname, './.env') });
-
-module.exports = process.env; // Exporta las variables de entorno
