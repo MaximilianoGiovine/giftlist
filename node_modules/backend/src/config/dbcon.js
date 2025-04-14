98156cdda9612bf381c2df9a6e8fef861e1bd128
Maximiliano Ariel Giovine <maxiagiovine@gmail.com>
2025-04-14 08:35:07 -0300

    alo

--- node_modules/backend/src/config/dbcon.js
+++ node_modules/backend/src/config/dbcon.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config(); // Carga las variables de entorno
 
-mongoose.set('strictQuery', true); // O false, según lo que prefieras
-
 const dbcon = async () => {
     try {
         const mongoUri = process.env.MONGO_URI; // Usa la variable de entorno
         if (!mongoUri) {
             throw new Error('MONGO_URI no está definida');
         }
+        mongoose.set('strictQuery', true); // Configuración para evitar la advertencia de Mongoose
         await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Connected to MongoDB');
     } catch (error) {
